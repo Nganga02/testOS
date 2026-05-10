a3a543353f3953a2bed6bfd041375b6a44588cfe
TheTechLeadKE <thetechleadke@gmail.com>
2026-05-10 14:53:53 +0300

    added a minimal serial driver

---
+++ run.sh
@@ -0,0 +1,16 @@
+#exec > /dev/null 2>&1
+
+C4C -i src/main.c4 -o main -t x86_64-baremetal-gnuC --emit-c --asmfiles loader1,loader2,x86 > /dev/null
+make
+make clean
+
+#qemu-system-x86_64 kernel.bin -net user -net nic,model=pcnet -device usb-ehci,id=ehci -device qemu-xhci,id=uhci
+
+qemu-system-x86_64 \
+    kernel.bin \
+    -net user \
+    -net nic,model=pcnet \
+    -device usb-ehci,id=ehci \
+    -device qemu-xhci,id=uhci \
+    -serial stdio
+
